f9e7c83aef3f7955101d1dce1c27a10c51975d0e
Jeffrey S Varghese <jeff0716042@gmail.com>
2023-06-19 23:28:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './utils/style.css';
+import Die from './components/Die';
+// import { ConnectionStates } from 'mongoose';
+import {nanoid} from "nanoid"
 function App() {
+  const[dice,setDice] = React.useState(allNewDice())
+
+  const diceElements = dice.map(
+    (x)=>{
+      return <Die key={x.id}value={x.value} isHeld={x.isHeld}/>
+    }
+  )
+  function allNewDice(){
+    const dice_array=[]
+    for(let i=0;i<10;i++)
+    {
+      dice_array.push({
+        id:nanoid(),
+        value: Math.floor(Math.random() * (6 - 1 + 1) ) + 1,
+        isHeld: true
+      });
+    }
+    return dice_array
+  }
+
+  function handleRollClick(){
+
+    setDice(allNewDice())
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='outer'>
+      <main>
+        <div className="dice-container">
+          
+          {diceElements}
+        </div>
+        <button className='Roll' onClick={handleRollClick}>Roll</button>
+        
+      </main>
+      </div>
+    
   );
 }
 
---
+++ src/components/Die.js
@@ -0,0 +1,13 @@
+import "../utils/style.css"
+
+export default function Die(props){
+    const styles={
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return(
+        <div className="die--face" style={styles}>
+            <h2 className="die--num">{props.value}</h2>
+        </div>
+        
+    )
+}
